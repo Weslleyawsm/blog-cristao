bc866bc64670fd788fb177d4e922916971131a76
Weslleyawsm <weslleysheshe@gmail.com>
2025-11-07 18:29:20 -0300

    Corrigindo URLs da API para produção

--- src/com/blogcristao/util/ConexaoDB.java
+++ src/com/blogcristao/util/ConexaoDB.java
@@ -4,12 +4,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoDB {
-    private static final String ENDPOINT = "prancheta-db.cgz4mmcgy3ns.us-east-1.rds.amazonaws.com";
-    private static final String PORTA = "3306";
-    private static final String DATABASE = "blogcristao";
-    private static final String USER = "admin";
-    private static final String PASSWORD = "awsm1944";
+    private static final String ENDPOINT = System.getenv("DB_HOST") != null
+            ? System.getenv("DB_HOST")
+            : "prancheta-db.cgz4mmcgy3ns.us-east-1.rds.amazonaws.com";
 
+    private static final String PORTA = System.getenv("DB_PORT") != null
+            ? System.getenv("DB_PORT")
+            : "3306";
+
+    private static final String DATABASE = System.getenv("DB_NAME") != null
+            ? System.getenv("DB_NAME")
+            : "blogcristao";
+
+    private static final String USER = System.getenv("DB_USER") != null
+            ? System.getenv("DB_USER")
+            : "admin";
+
+    private static final String PASSWORD = System.getenv("DB_PASSWORD") != null
+            ? System.getenv("DB_PASSWORD")
+            : "awsm1944";
     private static final String URL = "jdbc:mysql://" + ENDPOINT + ":" + PORTA + "/" + DATABASE
             + "?useSSL=true&serverTimezone=UTC";
     public static Connection getConexao() throws SQLException {
